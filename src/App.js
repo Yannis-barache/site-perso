5776782c8ba76c7739ceab4d5e7224b4d52bdeec
Yannis Barache <112858061+Yannis-barache@users.noreply.github.com>
2024-02-19 21:24:04 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Aside from './components/aside';
+import Body from "./components/body";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Portfolio de <code>BARACHE Yannis</code>
-        </p>
-        <a
-          className="App-link"
-          href="
-            https://github.com/Yannis-barache
-            "
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            Mon GitHub
-        </a>
-
-      </header>
+      <Aside />
+        <Body />
     </div>
   );
 }
---
+++ src/components/aside.js
@@ -0,0 +1,58 @@
+import {Fragment} from "react";
+import '../static/aside.css';
+
+
+
+
+
+function Aside() {
+    return (
+        <Fragment>
+            <aside>
+                <div className="aside">
+                    <div className="photo">
+                        <img
+                            src="https://avatars.githubusercontent.com/u/112858061?s=400&u=04f18100f23f395b6455758ea30cdd60aea60242&v=4"
+                            alt="profil"/>
+                    </div>
+                    <div className="description">
+                        <p>
+                            <code>BARACHE Yannis</code>
+                        </p>
+                    </div>
+                    <div className="lien">
+                        <a
+                            className="App-link"
+                            href="https://github.com/Yannis-barache"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <img src="https://techstack-generator.vercel.app/github-icon.svg"
+                                 alt="Logo de GitHub animé"
+                            />
+                            <p>GitHub</p>
+                        </a>
+                    </div>
+                </div>
+            </aside>
+            <label className="hamburger" >
+                <input type="checkbox"/>
+                <svg viewBox="0 0 32 32">
+                    <path className="line line-top-bottom"
+                          d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
+                    <path className="line" d="M7 16 27 16"></path>
+                </svg>
+            </label>
+            <div className="dropdown-menu">
+                <ul>
+                    <li>
+                        <a href="https://github.com/Yannis-barache" target="_blank"
+                           rel="noopener noreferrer"> Github</a>
+                    </li>
+                </ul>
+            </div>
+        </Fragment>
+    );
+}
+
+    export default Aside;
